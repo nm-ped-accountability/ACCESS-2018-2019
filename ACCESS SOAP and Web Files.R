0b63252176a02154839da31f10920206b222ddd5
yaoyunhz <yaoyunhz@gmail.com>
2019-07-05 14:28:54 -0600

    updated

--- ACCESS SOAP and Web Files.R
+++ ACCESS SOAP and Web Files.R
@@ -1,2 +1,25 @@
 
 ### ACCESS for ELLs SOAP and Web Files
+
+################################################################################
+### open and process raw data file
+################################################################################
+
+rm(list = ls())
+library(stringi)
+library(lubridate)
+library(Hmisc)
+library(tidyverse)
+
+# open files
+raw <- read.csv("ADDED DEMO NM_Summative_StudRR_File_2019-06-26.csv",
+                header = TRUE, stringsAsFactors = FALSE)
+dat <- raw
+
+schools <- read.csv("Master Schools 2019 V3.csv", 
+                    header = TRUE, stringsAsFactors = FALSE)
+
+################################################################################
+## recode variables
+
+# schnumb
