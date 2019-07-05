6464cff17ceec386c47d37b84591903b2a2fff29
yaoyunhz <yaoyunhz@gmail.com>
2019-07-05 16:25:39 -0600

    content added

--- ACCESS SOAP and Web Files.R
+++ ACCESS SOAP and Web Files.R
@@ -12,7 +12,7 @@ library(Hmisc)
 library(tidyverse)
 
 # open files
-raw <- read.csv("ADDED DEMO NM_Summative_StudRR_File_2019-06-26.csv",
+raw <- read.csv("ADDED DEMO NM_Summative_StudRR_File_2019-07_05.csv",
                 header = TRUE, stringsAsFactors = FALSE)
 dat <- raw
 
@@ -22,4 +22,27 @@ schools <- read.csv("Master Schools 2019 V3.csv",
 ################################################################################
 ## recode variables
 
-# schnumb
+# test_schnumb, STARS_schnumb
+dat$District.Number <- gsub("NM", "", dat$District.Number)
+dat$District.Number <- as.numeric(dat$District.Number)
+dat$test_schnumb <- dat$District.Number * 1000 + dat$School.Number
+dat$STARS_schnumb <- dat$S_DISTRICT_CODE * 1000 + dat$S_LOCATION_CODE
+
+# distcode
+dat$distcode <- dat$District.Number
+
+# distname
+dat$distname <- schools$distname[match(dat$distcode, schools$distcode)]
+
+# schcode
+dat$schcode <- dat$School.Number
+
+# schname
+dat$schname <- schools$schname[match(dat$test_schnumb, schools$schnumb)]
+
+# stid
+dat$stid <- dat$STUID
+range(dat$stid)
+
+# last
+dat$last <- dat$S_LASTNAME
