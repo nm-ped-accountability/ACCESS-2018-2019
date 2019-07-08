dc495b29fefa32e0f754232a31422a5944f76355
yaoyunhz <yaoyunhz@gmail.com>
2019-07-08 15:55:38 -0600

    updated

--- ACCESS SOAP and Web Files.R
+++ ACCESS SOAP and Web Files.R
@@ -23,11 +23,13 @@ schools <- schools[schools$ï..SY == 2019, ]
 ################################################################################
 ## recode variables
 
-# test_schnumb, STARS_schnumb
+# test_schnumb
 dat$District.Number <- gsub("NM", "", dat$District.Number)
 dat$District.Number <- as.numeric(dat$District.Number)
 dat$test_schnumb <- dat$District.Number * 1000 + dat$School.Number
 dat$STARS_schnumb <- dat$S_DISTRICT_CODE * 1000 + dat$S_LOCATION_CODE
+
+# STARS_schnumb
 
 # distcode
 dat$distcode <- dat$District.Number
@@ -62,3 +64,83 @@ dat$mi <- gsub("\\.", "", dat$mi)
 dat$mi <- gsub("0", "", dat$mi)
 dat$mi <- gsub("NULL", "", dat$mi)
 table(dat$mi)
+
+# dob
+dat$dob <- dat$S_DOB
+dat$dob <- mdy(dat$dob)
+str(dat$dob)
+
+# test_grade
+dat$test_grade_listen <- dat$Cluster...Listening
+table(dat$test_grade_listen)
+dat$test_grade_read <- dat$Cluster...Reading
+table(dat$test_grade_read)
+dat$test_grade_speak <- dat$Cluster...Speaking
+table(dat$test_grade_speak)
+dat$test_grade_write <- dat$Cluster...Writing
+table(dat$test_grade_write)
+
+# STARS_grade
+dat$STARS_grade <- dat$S_GRADE
+dat$STARS_grade <- gsub("KF", "0", dat$STARS_grade)
+dat$STARS_grade <- as.numeric(dat$STARS_grade)
+table(dat$STARS_grade)
+
+# eth
+dat$eth <- dat$S_ETNICITY
+table(dat$eth)
+table(dat$S_HISPANIC_INDICATOR)
+dat$eth[dat$S_HISPANIC_INDICATOR == "Yes"] <- "Hispanic"
+dat$eth[dat$eth == "Native Hawaiian or Other Pacific Islander"] <- "Asian"
+dat$eth[dat$eth == "Black or African American"] <- "African American"
+dat$eth[dat$eth == "American Indian/Alaskan Native"] <- "Native American"
+table(dat$eth)
+
+# gender
+dat$gender <- dat$S_GENDER
+table(dat$gender)
+
+# swd
+dat$swd <- dat$S_SPECIAL_ED
+table(dat$swd)
+dat$swd[dat$swd == "Y"] <- "Students with Disabilities"
+dat$swd[dat$swd == "N"] <- "Non SWD"
+table(dat$swd)
+
+# frl
+table(dat$S_FRLP)
+dat$frl[dat$S_FRLP == "F"] <- "Economically Disadvantaged"
+dat$frl[dat$S_FRLP == "R"] <- "Economically Disadvantaged"
+dat$frl[dat$S_FRLP == "N"] <- "Non ED"
+table(dat$frl)
+
+# ell
+dat$ell[dat$S_ELL_STATUS == "Y"] <- "English Learners"
+dat$ell[dat$S_ELL_STATUS == "N"] <- "Non EL"
+table(dat$ell)
+
+# migrant
+dat$migrant[dat$S_MIGRANT == "Y"] <- "Migrants"
+dat$migrant[dat$S_MIGRANT == "N"] <- "Non Migrant"
+dat$migrant[dat$S_MIGRANT == "NULL"] <- "Non Migrant"
+table(dat$migrant)
+
+# military
+# active, national guard, researve
+dat$military[dat$S_MILITARY == "Active"] <- "Military"
+dat$military[dat$S_MILITARY == "National Guard"] <- "Military"
+dat$military[dat$S_MILITARY == "Reserve"] <- "Military"
+dat$military[dat$S_MILITARY == "NULL"] <- "Non Military"
+table(dat$military)
+
+# homeless
+dat$homeless <- dat$S_HOMELESS
+table(dat$homeless)
+dat$homeless <- gsub("NULL", "Not Homeless", dat$homeless)
+table(dat$homeless)
+
+# foster
+table(dat$S_FOSTER)
+dat$foster[dat$S_FOSTER == "Y"] <- "Foster Care"
+dat$foster[dat$S_FOSTER == "NULL"] <- "Not Foster Care"
+table(dat$foster)
